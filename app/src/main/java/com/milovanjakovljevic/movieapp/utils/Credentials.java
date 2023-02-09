3df061741fe78461244c5a2f25ff17c76558a928
Milovan99 <milovanjakovljevic78@gmail.com>
2023-02-09 22:11:02 +0100

    Credentials for accesing movie db

---
+++ app/src/main/java/com/milovanjakovljevic/movieapp/utils/Credentials.java
@@ -0,0 +1,8 @@
+package com.milovanjakovljevic.movieapp.utils;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/";
+
+    public static final String PUBLIC_KEY="57b502c86db4507ba6e7c11ba85a2916";
+}
