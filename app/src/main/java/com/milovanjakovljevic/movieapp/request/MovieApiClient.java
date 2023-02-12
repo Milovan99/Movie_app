b4dfc394b5106d335a1fd1fccff6b02f41b8561a
Milovan99 <milovanjakovljevic78@gmail.com>
2023-02-12 22:49:51 +0100

    Executors

--- app/src/main/java/com/milovanjakovljevic/movieapp/request/MovieApiClient.java
+++ app/src/main/java/com/milovanjakovljevic/movieapp/request/MovieApiClient.java
@@ -3,9 +3,12 @@ package com.milovanjakovljevic.movieapp.request;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.milovanjakovljevic.movieapp.AppExecutors;
 import com.milovanjakovljevic.movieapp.modules.MovieModel;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class MovieApiClient {
 
@@ -28,4 +31,29 @@ public class MovieApiClient {
     public LiveData<List<MovieModel>> getMovies(){
         return mMovies;
     }
+
+
+    public void searchMoviesApi (){
+        final Future myHandler= AppExecutors.getInstance().networkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+                //Retrieve data from api
+
+
+
+            }
+        });
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                //Canceling Retrofit call
+                    myHandler.cancel(true);
+
+            }
+        }, 5000 , TimeUnit.MICROSECONDS);
+    }
+
+
+
 }
